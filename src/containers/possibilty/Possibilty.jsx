5680e5b8c0f37c7bf74195d7c60ac3e25086bcf1
ozgeonline <nisroc.414@hotmail.com>
2023-09-12 01:31:52 +0300

    added section

--- src/containers/possibilty/Possibilty.jsx
+++ src/containers/possibilty/Possibilty.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './possibilty.css'
+import possibiltyImage from '../../assets/possibility.png'
 
 const Possibilty = () => {
   return (
-    <div>
-      
+    <div className='gpt3__possibilty section__padding' id='possibilty'>
+      <div className='gpt3__possibilty-image'>
+        <img src={possibiltyImage} alt='possibilty'/>
+      </div>
+      <div className='gpt3__possibilty-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
 }
